ce7e642eb5fb759136ab6e21850a3126571194fd
Chaves <pedrochamar12@gmail.com>
2022-07-16 02:14:35 -0300

    :sparkles: Design statico da home concluído

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,37 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  public logo = "/assets/images/logo.png";
+  public vector = "/assets/images/vector.svg";
+  public items: any = [{
+    title: "Product",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",
+    button: "Play"
+  }, {
+    title: "Service", description: "Veritatis\n" +
+      "obcaecati enetur iure eius earum ut olestias architecto voluptate liquam\n" +
+      "nihil, eveniet aliquid culpa officia aut!", button: "See Documentation"
+  },{
+    title: "Contact", description: "Quo neque error repudiandae fuga?", button: "Call us"
+  }];
+  selectedIndex = 0;
+
+  @Input() indicators = true;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  selectItem(index: number): void {
+    this.selectedIndex = index;
+  }
+
+}
