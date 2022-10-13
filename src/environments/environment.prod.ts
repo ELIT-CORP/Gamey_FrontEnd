52767d5f035664f50c6d4dbea9990ddb91acd3af
Matheus Sena <61739336+S3nna@users.noreply.github.com>
2022-10-12 21:49:33 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
   production: true
+  firebaseConfig: {
+    apiKey: "AIzaSyDJRKfWSRVKRBOHxgkCrFKgAkrFcquzzy0",
+    authDomain: "gamey-d8c1f.firebaseapp.com",
+    databaseURL: "https://gamey-d8c1f-default-rtdb.firebaseio.com",
+    projectId: "gamey-d8c1f",
+    storageBucket: "gamey-d8c1f.appspot.com",
+    messagingSenderId: "244783056436",
+    appId: "1:244783056436:web:bd537569c5b049e2f6ec61",
+    measurementId: "G-NP52Z63NZK"
+  }
 };
